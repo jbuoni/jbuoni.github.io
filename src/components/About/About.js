8baf9bb598af658bff8aff81cfc3f5d6de49fe7c
jbuoni <jason.buoni@turner.com>
2019-08-19 21:27:46 -0400

    brobot

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -47,10 +47,10 @@ export default class About extends Component {
                 <TopHeader text="About"/>
                 <Fade top>
                     <div className="about-middle">
-                        <Icon faIcon="fas fa-chalkboard-teacher" title="Mentor" exp="I been fortunate enough to be able to give back to the development community through mentoring junior developers. Seeing their success has been the most rewarding part of my career."/>
+                        <Icon faIcon="fas fa-chalkboard-teacher" title="Mentor" exp="I've been fortunate enough to be able to give back to the development community through mentoring junior developers. Seeing their success has been the most rewarding part of my career."/>
                         <Icon faIcon="fas fa-piggy-bank" title="Ambitious" exp="I learned early on that while knowledge can get you far, hard work gets you further. In my role as a Senior Developer, I take a lead from the trenches approach and am never afraid to roll up my sleeves and get to work."/>
-                        <Icon faIcon="fas fa-layer-group stack" title="Full Stack" exp="From UI to Databases, I like to be involved in the full process."/>
-                        <Icon faIcon="fas fa-hat-wizard hat" title="Multiple Hats" exp="Context switching is one of my stronger skills. I prefer can do a bit of everything."/>
+                        <Icon faIcon="fas fa-layer-group stack" title="Full Stack" exp="From Front-end to Back-end and everything in between, I like to be involved in the full development process. By looking at the big-picture, I can anticipate potential problems and continuously improve projects on the fly."/>
+                        <Icon faIcon="fas fa-robot robot" title="Extracurriculars" exp="In addition to the core CS skill set and high-level development design that I utilize in my day-to-day,through personal projects I have become well versed in the facets of cutting-edge concepts such as machine-learning, cyber-security, and cloud computing."/>
                     </div>
                 </Fade>
                 <div className="about-bottom">
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { onBitbucketClick, onFacebookClick, onGithubClick, onLinkedInClick } from '../utils/navUtils';
+import {
+    onBitbucketClick,
+    onFacebookClick,
+    onGithubClick,
+    onLinkedInClick,
+    onStackOverflowClick
+} from '../utils/navUtils';
 
 export default class Footer extends Component {
     constructor(props) {
@@ -18,10 +24,11 @@ export default class Footer extends Component {
             <>
                 <div className="footer">
                     <span onClick={this.onEmailButtonClick}><i className="far fa-envelope-open fa-2x icon"></i></span>
-                    <span onClick={onFacebookClick}><i className="fab fa-facebook fa-2x icon"></i></span>
                     <span onClick={onLinkedInClick}><i className="fab fa-linkedin-in fa-2x icon"></i></span>
                     <span onClick={onGithubClick}><i className="fab fa-github-alt fa-2x icon" aria-hidden="true"></i></span>
                     <span onClick={onBitbucketClick}><i className="fab fa-bitbucket fa-2x icon" aria-hidden="true"></i></span>
+                    <span onClick={onStackOverflowClick}><i className="fab fa-stack-overflow fa-2x icon"></i></span>
+                    <span onClick={onFacebookClick}><i className="fab fa-facebook fa-2x icon"></i></span>
                 </div>
             </>
         );
