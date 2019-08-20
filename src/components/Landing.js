56910be854fa149ad5e991240adb8fdfa87e1625
jbuoni <jason.buoni@turner.com>
2019-08-19 20:36:16 -0400

    Removed arrow

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,14 +10,11 @@ export default class Landing extends Component {
     constructor(props) {
         super(props);
         this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-        this.toggleButtonHover = this.toggleButtonHover.bind(this);
     }
 
     state = {
         height: 0,
-        width: 0,
-        hover: false,
-        firstHover: true
+        width: 0
     };
 
     componentDidMount() {
@@ -33,18 +30,7 @@ export default class Landing extends Component {
         this.setState({ width: window.innerWidth, height: window.innerHeight });
     }
 
-    toggleButtonHover(){
-        this.setState({
-            hover: !this.state.hover,
-            firstHover: true
-        })
-    }
-
     render() {
-        const { hover, firstHover } = this.state;
-
-        const transform = hover && 'rotateZ(90deg) !important' || !hover && !firstHover && 'rotateZ(-90deg) !important' || null;
-
         return (
             <>
                 <div className="landing-image" style={{ height: this.state.height, width: this.state.width }}></div>
@@ -56,12 +42,8 @@ export default class Landing extends Component {
                         <span >Software developer <br/> Avid mentor <br/> Pet enthusiast <br/></span>
                         <div className="col-md-3 col-sm-3 col-xs-6 landing-link-container"
                              onMouseEnter={this.toggleButtonHover} onMouseLeave={this.toggleButtonHover}>
-                            <Link to="/about" className="btn btn-sm animated-button victoria-one">
-                                View More <i className="fa fa-arrow-right landing-arrow"
-                                             style={{transform: transform}}></i>
-                            </Link>
+                            <Link to="/about" className="btn btn-sm animated-button victoria-one">View More</Link>
                         </div>
-                        <span className="hack">hack</span>
                     </div>
                 </Typist>
             </>
