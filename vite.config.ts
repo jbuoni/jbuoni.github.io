9a9ff2fbbb78808ee43726b4802793ec7c4c613b
Jason Buoni <ssride360@gmail.com>
2025-03-06 00:56:10 -0500

    misc

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// const ASSET_URL = process.env.ASSET_URL || ''
+
 // https://vite.dev/config/
 export default defineConfig({
+  // base: `${ASSET_URL}/dist/`,
   plugins: [react()],
 })
